5352a09459429116f8a26460fc8016c8b762ee75
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-08-25 22:46:03 +0530

    Added cities comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
+
 import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import Homepage from "./pages/Homepage"
@@ -6,13 +8,13 @@ import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
-import { useEffect, useState } from "react"
+import CountryList from "./components/CountryList"
 
 const BASE_URL = 'http://localhost:8000'
 
 function App() {
 
-  const [cities, setCities] = useState({});
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   console.log(cities,isLoading)
 
@@ -44,7 +46,7 @@ function App() {
         <Route path="app" element={<AppLayout />} >
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,24 @@
+import Spinner from './Spinner'
+import styles from './CountryList.module.css'
+import Message from './Message';
+import CountryItem from './CountryItem';
+
+function CountryList({ cities, isLoading }) {
+    if (isLoading) return <Spinner />;
+
+    if (!cities.length) return <Message message='Add your first city by clicking on a city on the map' />
+
+    const countries = Array.from(
+        new Map(cities.map(city => [city.country,{country : city.country, emoji : city.emoji}])).values()
+    );
+
+    console.log(countries)
+    return (
+        <ul className={styles.countryList}>
+            {countries.map((country) =>
+                <CountryItem country={country} key={country.country} />)}
+        </ul>
+    )
+}
+
+export default CountryList
